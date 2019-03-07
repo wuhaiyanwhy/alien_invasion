c174490a8fff6e8478b6a5df00d3561e6aba53e3
WuHaiYan <wuhaiyan@iqianggou.com>
2019-03-07 09:52:11 +0800

    1

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf 
+
+
+def run_game():
+    pygame.init()
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
+    
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建飞船
+    ship = Ship(ai_settings,screen)
+
+    #设置背景色
+    # bg_color = (230,230,230)
+
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+
+run_game()
+
+
+
